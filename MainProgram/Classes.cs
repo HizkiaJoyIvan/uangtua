99fe2cd679fc935a1fb6cf8819271c49ed8fc374
Raditya CBP <raditya.com@gmail.com>
2023-09-18 11:30:26 +0700

    classes

---
+++ MainProgram/Classes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProgram
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+    }
+    public class Income
+    {
+        public Income() { }
+    }
+    public class Expense
+    {
+        public Expense() { }
+    }
+    public class ExpenseCategory
+    {
+        public ExpenseCategory() { }
+    }
+
+    public class IncomeCategory
+    {
+        public IncomeCategory()
+        {
+        }
+    }
+}
