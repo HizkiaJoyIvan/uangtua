8209f1f8f1e4600bc048547adc484a90e98ddc08
Joy Ivan <joyivan6@gmail.com>
2023-09-21 20:56:33 +0700

    add initial class diagram

--- MainProgram/Classes.cs
+++ MainProgram/Classes.cs
@@ -9,37 +9,40 @@ namespace MainProgram
     public class User
     {
         public User(
-        int userid,
-        string username,
-        string email,
-        string contacs,
-        string password)
+            int userid,
+            string username,
+            string email,
+            string contacs,
+            string password)
         {
-           UserID = userid;
-           Username = username;
-           Email = email;
-           Contacs = contacs;
-           Password = password;
-           
+            UserID = userid;
+            Username = username;
+            Email = email;
+            Contacs = contacs;
+            Password = password;
+            Incomes = new List<Income>();
+            Expenses = new List<Expense>();
         }
         public int UserID { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string Contacs { get; set; }
         public string Password { get; set; }
+        public ICollection<Income> Incomes { get; set; }
+        public ICollection<Expense> Expenses { get; set; }
     }
 
     public class Income
     {
         public Income(
-        int incomeid,
-        string title,
-        int catagoryid,
-        int amount,
-        string type,
-        string description,
-        DateTime createAt,
-        int userid)
+            int incomeid,
+            string title,
+            int catagoryid,
+            int amount,
+            string type,
+            string description,
+            DateTime createAt,
+            int userid)
         {
             IncomeId = incomeid;
             Title = title;
@@ -58,9 +61,9 @@ namespace MainProgram
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
         public int UserId { get; set; }
+        public IncomeCategory IncomeCategory { get; set; }
+        public User User { get; set; }
     }
-}
-
     public class Expense
     {
         public int ExpenseId { get; set; }
@@ -72,6 +75,8 @@ namespace MainProgram
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
         public int UserId { get; set; }
+        public ExpenseCategory ExpenseCategory { get; set; }
+        public User User { get; set; }
 
         public Expense(
             int expenseId,
@@ -98,13 +103,38 @@ namespace MainProgram
 
     public class ExpenseCategory
     {
-        public ExpenseCategory() { }
+        public int ExpenseCategoryId { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public ICollection<Expense> Expenses { get; set; }
+        public ExpenseCategory(
+            int expenseCategoryId,
+            string name,
+            string slug
+            )
+        {
+            ExpenseCategoryId = expenseCategoryId;
+            Name = name;
+            Slug = slug;
+        }
     }
 
     public class IncomeCategory
     {
-        public IncomeCategory()
+        public int IncomeCategoryId { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public ICollection<Income> Incomes { get; set; }
+        public IncomeCategory(
+                int incomeCategoryId,
+                string name,
+                string slug
+                )
         {
+            IncomeCategoryId = incomeCategoryId;
+            Name = name;
+            Slug = slug;
         }
     }
 }
+
